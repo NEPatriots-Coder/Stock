2804461b241bf6e932e5d6e3a96949267c76ed23
NEPatriots-Coder <144710540+NEPatriots-Coder@users.noreply.github.com>
2023-10-05 14:20:22 +0000

    "Several new information added in this commit"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import yfinance as yf
                                 
                                 
 st. title("Doosan Stock Price Analysis")
+st.write("""### ***All information provided by Yahoo Finance***""")
 st.write(""" ## Stock Price of Doosan Corp
 Shown is *Opening Price*, *Closing Price*, and *Daily Volume*""")
 
@@ -17,3 +18,25 @@ st.write("## Closing Price")
 st.line_chart(tickerDf.Close)  
 st.write("## Daily Volume")
 st.line_chart(tickerDf.Volume) 
+
+Bobcat = yf.Ticker("000150.KS")
+Bobcat.info 
+
+st.write("### The following will list a few summary statistics about Doosan Bobcat")
+Bobcat.actions
+Bobcat.dividends
+Bobcat.splits
+Bobcat.capital_gains
+
+
+Bobcat.major_holders
+
+Bobcat.earnings_dates
+st.write("## Doosan Income Statement")
+Bobcat.income_stmt
+st.write("## Doosan Balance Sheet")
+Bobcat.balancesheet
+st.write("## Doosan Cash Flow")
+Bobcat.cashflow
+
+hist = Bobcat.history
